6f2ee307cf42475f65a6808ab7e85adce0f6f197
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2020-09-07 22:13:24 +0000

    not the best

--- pratice_web/Object.js
+++ pratice_web/Object.js
@@ -1,9 +1,39 @@
-var user = {
-    email: "coded@codedStudios.com",
-    pass: "fgyr747tygyf",
-    login() {
-        console.log(this.email, this.pass);
-    }
-};
+// Object Litterals
 
-user.login();
+// <---- Not the best method--->
+// var userone = {
+//     email: "coded@codedStudios.com",
+//     pass: "fgyr747tygyf",
+//     name: "coded",
+//     login() {
+//         console.log(this.email, this.pass);
+//     },
+
+//     logout() {
+//         console.log(this.name, this.pass);
+//     }
+// };
+
+// var usertwo = {
+//     email: "coded23@codedStudios.com",
+//     pass: "fgyr74237tygyf",
+//     name: "codedSpartant",
+//     login() {
+//         console.log(this.email, this.pass);
+//     },
+
+//     logout() {
+//         console.log(this.name, this.pass);
+//     }
+// };
+
+
+
+
+// userone.login();
+// userone.logout();
+// usertwo.login();
+// usertwo.logout();
+
+
+// <----------- using Classes --------->
